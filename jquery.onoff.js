ca1d50c90a215cc83d9893cdf37c18a52bf8035c
Chris Lewis <chris@thegodcode.net>
2011-12-19 13:55:30 -0500

    jq js

---
+++ jquery.onoff.js
@@ -0,0 +1,27 @@
+(function($) {
+  var cssClassPane = "toggle_pane";
+  
+  var methods = {
+    replaceCheckboxes: function() {
+      this.find("input[type=checkbox]").each(function() {
+        var toggle = $('<div class="onoff"><div class="toggle_pane on_state"><div class="on">on</div><div class="handle">&nbsp;</div><div class="off">off</div></div></div>');
+        var pane = $(toggle.children("." + cssClassPane));
+        $(this).before(toggle);
+        if(! this.checked) pane.addClass("off_state");
+        var ckbox = this;
+        $(ckbox).hide();
+        pane.click(function(e) {
+          ckbox.checked = $(this).hasClass("off_state");
+          $(this).toggleClass("off_state");
+        });
+      });
+    }
+  };
+
+  $.fn.onOff =
+    function(method) {
+      if(methods[method]) {
+        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+      }
+    };
+})(jQuery);
