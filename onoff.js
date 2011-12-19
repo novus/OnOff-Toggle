713639bb0e9700cc57fc35fdab649145a09cfa35
Chris Lewis <chris@thegodcode.net>
2011-12-19 10:58:40 -0500

    revised replaceCheckboxes to match ckboxes under current jq object

--- onoff.js
+++ onoff.js
@@ -1,6 +1,6 @@
 (function($) {
   $.fn.replaceCheckboxes = function() {
-    this.each(function() {
+    this.find("input[type=checkbox]").each(function() {
       var toggle = $('<div class="onoff"><div class="toggle_pane on_state"><div class="on">on</div><div class="handle">&nbsp;</div><div class="off">of\
 f</div></div></div>');
       var pane = $(toggle.children(".toggle_pane"));
