906810b896c6645f6df6ded99e00705d5fe1e1e3
Chris Lewis <chris@thegodcode.net>
2011-12-23 13:18:22 -0500

    new options useLabel and onClickEvent thx @max4f

--- js/jquery.onoff.js
+++ js/jquery.onoff.js
@@ -1,41 +1,61 @@
 (function($) {
-  var cssClassPane = "nv-pane";
-  
-  var methods = {
-    init: function(options) {
+    var cssClassPane = "nv-pane";
 
-      var settings = $.extend({
-        "bindClick": false
-      }, options);
+    var methods = {
+        init: function(options) {
 
-      this.find("input[type=checkbox]").each(function() {
-        /* Create the toggle control in the document. */
-        var toggle = $('<div class="nv-toggle"><div class="nv-pane nv-on-state"><div class="nv-on">on</div><div class="nv-handle">&nbsp;</div><div class="nv-off">off</div></div></div>');
-        var pane = $(toggle.children("." + cssClassPane));
-        $(this).before(toggle);
-        /* Bind the state to the peer checkbox, respecting the default value. */
-        if(! this.checked) pane.addClass("nv-off-state");
-        var ckbox = this;
-        $(ckbox).hide();
-        pane.click(function(e) {
-          ckbox.checked = $(this).hasClass("nv-off-state");
-          $(this).toggleClass("nv-off-state");
-        });
-        /* If requested, bind to the click event of the peer (enables label clicks). */
-        if(settings.bindClick)
-          $(ckbox).click(function() {
-            pane.toggleClass("nv-off-state");
-          });
-      });
-    }
-  };
+            var settings = $.extend({
+                "bindClick": false,
+                "useLabel": false,
+                "onClickEvent": ""
+            }, options);
 
-  $.fn.onOff =
-    function(method) {
-      if(methods[method]) {
-        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-      } else if ( typeof method === 'object' || ! method ) {
-        return methods.init.apply(this, arguments);
-      } else $.error("Method " + method + " does not exist!");
+            this.find("input[type=checkbox]").each(function() {
+                /* Create the toggle control in the document. */
+                var ckbox = this;
+                var defaultLabels = ['on', 'off'];
+                var labels = (function() {
+                    if(settings.useLabel) {
+                        var label = $("label[for='" + $(ckbox).attr("id") + "']").html();
+                        if (!!label) return [label, label]; else return defaultLabels;
+                    } else return defaultLabels;
+                })();
+                var onLabel = labels[0];
+                var offLabel = labels[1];
+
+                var toggle = $(
+                    '<div class="nv-toggle">' +
+                        '<div class="nv-pane nv-on-state">' +
+                        '<div class="nv-on">' + onLabel + '</div>' +
+                        '<div class="nv-handle">&nbsp;</div>' +
+                        '<div class="nv-off">' + offLabel + '</div>' +
+                        '</div>' +
+                        '</div>');
+                var pane = $(toggle.children("." + cssClassPane));
+                $(this).before(toggle);
+                /* Bind the state to the peer checkbox, respecting the default value. */
+                if (! this.checked) pane.addClass("nv-off-state");
+                $(ckbox).hide();
+                pane.click(function(e) {
+                    ckbox.checked = $(this).hasClass("nv-off-state");
+                    $(this).toggleClass("nv-off-state");
+                    if (!!settings.onClickEvent) $(ckbox).trigger(settings.onClickEvent);
+                });
+                /* If requested, bind to the click event of the peer (enables label clicks). */
+                if (settings.bindClick)
+                    $(ckbox).click(function() {
+                        pane.toggleClass("nv-off-state");
+                    });
+            });
+        }
     };
+
+    $.fn.onOff =
+        function(method) {
+            if (methods[method]) {
+                return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+            } else if (typeof method === 'object' || ! method) {
+                return methods.init.apply(this, arguments);
+            } else $.error("Method " + method + " does not exist!");
+        };
 })(jQuery);
